a179e7128ad9b90a4785bfdda65b86f30a9e6416
manukko <45536493+manukko@users.noreply.github.com>
2025-04-16 18:01:45 +0200

    schemas in separate folder

---
+++ src/schemas/users.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel
+
+from src.schemas.todos import TodoModel
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserModel(BaseModel):
+    username: str
+    email: str
+    created_at: datetime
+    updated_at: datetime
+    role: str
+    todos: List[TodoModel]
