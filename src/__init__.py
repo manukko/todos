489b5be4eecd62ff22bf34e36dcf832dd159f409
manukko <45536493+manukko@users.noreply.github.com>
2025-05-05 12:58:26 +0200

    add trusted host middleware

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,8 @@ from src.db.models import init_db
 from src.routes.todos import router as todo_router
 from src.routes.users import router as user_router
 from contextlib import asynccontextmanager
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from env import DOMAIN
 
 
 @asynccontextmanager
@@ -15,5 +17,8 @@ async def lifespan(app: FastAPI):
 
 version = "v1"
 app = FastAPI(title="todos api", version=version, lifespan=lifespan)
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1", DOMAIN])
+
 app.include_router(router=todo_router, prefix=f"/api/{version}/todos")
 app.include_router(router=user_router, prefix=f"/api/{version}/users")
